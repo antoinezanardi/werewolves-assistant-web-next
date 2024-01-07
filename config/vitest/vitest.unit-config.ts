e8dd71154c22731c39bf68c226479199a1bde1c9
Antoine Zanardi <antoine.zanardi@epitech.eu>
2024-01-07 17:56:18 +0100

    feat(tests): set up unit tests ecosystem (#6)

---
+++ config/vitest/vitest.unit-config.ts
@@ -0,0 +1,37 @@
+import { defineVitestConfig } from "@nuxt/test-utils/config"
+
+export default defineVitestConfig({
+  test: {
+    watch: false,
+    include: ["./tests/unit/**/*.spec.ts"],
+    exclude: [
+      "nuxt.config.ts",
+      "node_modules/**",
+      "config/**/*.ts",
+    ],
+    coverage: {
+      exclude: [
+        "tests/**/*",
+        "node_modules/**/*",
+        "config/**/*",
+      ],
+      include: [
+        "app.vue",
+        "components/**/*.[vue|ts]",
+        "composables/**/*.ts",
+      ],
+      reportsDirectory: "./tests/unit/coverage",
+      all: true,
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      }
+    },
+    globals: true,
+    clearMocks: true,
+    mockReset: true,
+    restoreMocks: true,
+  }
+});
