b7ab74c6b485df735f8a961fd4a56839dd4290bf
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-01-09 00:13:50 +0100

    feat(eslint): set up eslint with new flat config

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    Hello World!
   </div>
 </template>
--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,7 @@ import { ESLINT_TESTS_CONFIG } from "./config/eslint/config/eslint.tests-config.
 import { ESLINT_STYLISTIC_CONFIG } from "./config/eslint/config/eslint.stylistic-config.js";
 import { ESLINT_IMPORT_CONFIG } from "./config/eslint/config/eslint.import-config.js";
 import { ESLINT_CONFIG_FILES_CONFIG } from "./config/eslint/config/eslint.config-files-config.js";
+import { ESLINT_VUE_CONFIG } from "./config/eslint/config/eslint.vue-config.js";
 
 const baseDirectory = path.dirname(fileURLToPath(import.meta.url));
 const flatCompat = new FlatCompat({ baseDirectory });
@@ -22,10 +23,12 @@ export default [
       "public/",
     ],
   },
-  ...flatCompat.plugins("import"),
+  ...flatCompat.plugins("import", "vue"),
+  ...flatCompat.extends("plugin:vue/vue3-recommended"),
   ESLINT_BASE_CONFIG,
   ESLINT_TYPESCRIPT_CONFIG,
   ESLINT_IMPORT_CONFIG,
+  ESLINT_VUE_CONFIG,
   ESLINT_TESTS_CONFIG,
   ESLINT_STYLISTIC_CONFIG,
   ESLINT_CONFIG_FILES_CONFIG,
