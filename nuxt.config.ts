56592f57a32fe7a156dd1925bb272f3e403ca702
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-01-10 21:28:26 +0100

    feat(prime-vue): implement prime vue as ui suite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  modules: ["@nuxt/test-utils/module"],
+  modules: [
+    "@nuxt/test-utils/module",
+    "nuxt-primevue",
+  ],
+  css: ["primevue/resources/themes/bootstrap4-dark-blue/theme.css"],
+  primevue: {
+    components: {
+      prefix: "VuePrime",
+      include: ["Button"],
+    },
+    directives: {
+      prefix: "p-",
+      include: ["Tooltip"],
+    },
+    composables: { include: [] },
+  },
   devtools: { enabled: true },
 });
