e3139bc3776a4ac589c05d5b3ad238405205e9a6
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-01-12 15:06:00 +0100

    feat(cucumber): cucumber setup for e2e tests

--- tests/acceptance/features/support/hooks.ts
+++ tests/acceptance/features/support/hooks.ts
@@ -1,10 +1,21 @@
-import { Before } from "@cucumber/cucumber";
-import { createPage, setup } from "@nuxt/test-utils/e2e";
+import { After, AfterAll, Before } from "@cucumber/cucumber";
+import { createPage, createTest } from "@nuxt/test-utils/e2e";
 
 import type { CustomWorld } from "~/tests/acceptance/shared/types/word.types";
 
+const { beforeEach, afterEach, afterAll, setup } = createTest({ runner: "vitest", server: true });
+
 Before(async function(this: CustomWorld): Promise<void> {
-  await setup({ runner: "vitest" });
+  // setup();
+  beforeEach();
 
   this.page = await createPage("/");
+});
+
+After((): void => {
+  afterEach();
+});
+
+AfterAll((): void => {
+  afterAll();
 });
