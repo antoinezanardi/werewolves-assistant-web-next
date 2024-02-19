cd88926cd09cfeb915eeb8809a74d82f3fe15456
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-02-19 15:28:11 +0100

    fix(e2e): visual comparison less strict

--- tests/acceptance/features/playwright/step-definitions/screenshots/helpers/playwright-screenshots.helpers.ts
+++ tests/acceptance/features/playwright/step-definitions/screenshots/helpers/playwright-screenshots.helpers.ts
@@ -1,12 +1,10 @@
 import type { Page } from "playwright-core";
 
 import { DEFAULT_PLAYWRIGHT_PAGE_SCREENSHOT_OPTIONS } from "~/tests/acceptance/features/playwright/step-definitions/screenshots/constants/playwright-screenshots.constants";
-import { ACCEPTANCE_TESTS_PATH_SCREENSHOTS_PATH } from "~/tests/acceptance/shared/constants/acceptance.constants";
 import type { CustomWorld } from "~/tests/acceptance/shared/types/word.types";
 
-async function saveFullPageScreenshot(page: Page, screenshotName: string): Promise<void> {
-  const screenshotPath = `${ACCEPTANCE_TESTS_PATH_SCREENSHOTS_PATH}/${screenshotName}.png`;
-  console.info(`The snapshot with name "${screenshotName}" does not exist. Creating a new snapshot.`);
+async function saveFullPageScreenshot(page: Page, screenshotPath: string): Promise<void> {
+  console.info(`The snapshot with path "${screenshotPath}" does not exist. Creating a new snapshot.`);
   await page.screenshot({
     path: screenshotPath,
     ...DEFAULT_PLAYWRIGHT_PAGE_SCREENSHOT_OPTIONS,
--- tests/acceptance/features/playwright/step-definitions/screenshots/playwright-screenshots.then-steps.ts
+++ tests/acceptance/features/playwright/step-definitions/screenshots/playwright-screenshots.then-steps.ts
@@ -11,9 +11,14 @@ import { ACCEPTANCE_TESTS_PATH_SCREENSHOTS_PATH } from "~/tests/acceptance/share
 import type { CustomWorld } from "~/tests/acceptance/shared/types/word.types";
 
 Then(/^the page should match the snapshot with name "(?<name>.+)"$/u, async function(this: CustomWorld, name: string): Promise<void> {
-  const screenshotPath = `${ACCEPTANCE_TESTS_PATH_SCREENSHOTS_PATH}/${name}.png`;
+  const screenshotPath = `${ACCEPTANCE_TESTS_PATH_SCREENSHOTS_PATH}/${process.platform}/${name}.png`;
+  const screenshot = PNG.sync.read(await this.page.screenshot(DEFAULT_PLAYWRIGHT_PAGE_SCREENSHOT_OPTIONS));
   if (!existsSync(screenshotPath)) {
-    await saveFullPageScreenshot(this.page, name);
+    this.attach(PNG.sync.write(screenshot), {
+      mediaType: "image/png",
+      fileName: `[NEW] - ${name}.png`,
+    });
+    await saveFullPageScreenshot(this.page, screenshotPath);
 
     return;
   }
@@ -22,7 +27,6 @@ Then(/^the page should match the snapshot with name "(?<name>.+)"$/u, async func
     width: baseScreenshot.width,
     height: baseScreenshot.height,
   });
-  const screenshot = PNG.sync.read(await this.page.screenshot(DEFAULT_PLAYWRIGHT_PAGE_SCREENSHOT_OPTIONS));
   const pixelMatchOptions: PixelmatchOptions = { threshold: 0.8 };
   const pixelDiff = pixelMatch(screenshot.data, baseScreenshot.data, diffScreenshot.data, screenshot.width, screenshot.height, pixelMatchOptions);
 
