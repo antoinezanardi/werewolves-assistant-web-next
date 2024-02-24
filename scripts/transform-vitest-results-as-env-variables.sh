e78403f6a23d3f7c15047052ebaf7309e3c1fa6a
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-02-24 12:25:39 +0100

    ci(badges): generate badges on release creation

--- scripts/transform-vitest-results-as-env-variables.sh
+++ scripts/transform-vitest-results-as-env-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 output=$(cat)
 
-tests_count=$(echo "$output" | awk '/Tests:/{print $2}')
+tests_count=$(echo "$output" | awk '/Tests/{print $2}')
 
 statements_count=$(echo "$output" | awk '/Statements/{print $(NF-1)}')
 branches_count=$(echo "$output" | awk '/Branches/{print $(NF-1)}')
@@ -13,14 +13,14 @@ branches_percent=$(echo "$output" | awk '/Branches/{print $3}')
 functions_percent=$(echo "$output" | awk '/Functions/{print $3}')
 lines_percent=$(echo "$output" | awk '/Lines/{print $3}' | tr -d '\nLine')
 
-echo "JEST_TESTS_COUNT=$tests_count"
+echo "VITEST_TESTS_COUNT=$tests_count"
 
-echo "JEST_STATEMENTS_COUNT=$statements_count"
-echo "JEST_BRANCHES_COUNT=$branches_count"
-echo "JEST_FUNCTIONS_COUNT=$functions_count"
-echo "JEST_LINES_COUNT=$lines_count"
+echo "VITEST_STATEMENTS_COUNT=$statements_count"
+echo "VITEST_BRANCHES_COUNT=$branches_count"
+echo "VITEST_FUNCTIONS_COUNT=$functions_count"
+echo "VITEST_LINES_COUNT=$lines_count"
 
-echo "JEST_STATEMENTS_PERCENT=$statements_percent"
-echo "JEST_BRANCHES_PERCENT=$branches_percent"
-echo "JEST_FUNCTIONS_PERCENT=$functions_percent"
-echo "JEST_LINES_PERCENT=\"${lines_percent:1}\""
+echo "VITEST_STATEMENTS_PERCENT=$statements_percent"
+echo "VITEST_BRANCHES_PERCENT=$branches_percent"
+echo "VITEST_FUNCTIONS_PERCENT=$functions_percent"
+echo "VITEST_LINES_PERCENT=${lines_percent:1}"
