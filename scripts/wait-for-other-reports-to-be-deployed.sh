6100a4f7afe6796d00728ec712970e5f8f19d32b
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-02-28 21:12:02 +0100

    ci(reports): reports in separate workflow

--- scripts/wait-for-other-reports-to-be-deployed.sh
+++ scripts/wait-for-other-reports-to-be-deployed.sh
@@ -5,17 +5,20 @@ workflow_name="📑 Deploy Reports to GitHub Pages"
 attempts=15
 interval_seconds=15
 
-for ((i = 1; i <= $attempts; i++)); do
-  runs=$(gh run list -R "$repo" -w "$workflow_name" --json status -s "in_progress" --limit 2 | jq ". | length")
+for ((i = 1; i <= attempts; i++)); do
+  in_progress_runs=$(gh run list -R "$repo" -w "$workflow_name" --json status -s "in_progress" --limit 2 | jq ". | length")
+  waiting_runs=$(gh run list -R "$repo" -w "$workflow_name" --json status -s "waiting" --limit 2 | jq ". | length")
 
-  if [ "$runs" -le 1 ]; then
+  total_runs=$((in_progress_runs + waiting_runs))
+
+  if [ "$total_runs" -eq 1 ]; then
     echo "✅ Found one active run (this run itself). Exiting with code 0."
     exit 0
-  elif [ "$runs" -eq 0 ]; then
+  elif [ "$total_runs" -eq 0 ]; then
     echo "✅ No active runs found. Exiting with code 0."
     exit 0
   else
-    echo "🕘 Found $runs active runs. Waiting for $interval_seconds seconds before the next attempt ($i/$attempts)."
+    echo "🕘 Found $total_runs active runs. Waiting for $interval_seconds seconds before the next attempt ($i/$attempts)."
     sleep $interval_seconds
   fi
 done
