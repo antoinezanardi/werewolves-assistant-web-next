9b8f9d3fd4a5cdc36cdd36637e49c13dc484b6b6
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-02-28 21:34:53 +0100

    ci(reports): reports in separate workflow

--- scripts/wait-for-other-reports-to-be-deployed.sh
+++ scripts/wait-for-other-reports-to-be-deployed.sh
@@ -7,9 +7,9 @@ interval_seconds=15
 
 for ((i = 1; i <= attempts; i++)); do
   in_progress_runs=$(gh run list -R "$repo" -w "$workflow_name" --json status -s "in_progress" --limit 2 | jq ". | length")
-  waiting_runs=$(gh run list -R "$repo" -w "$workflow_name" --json status -s "waiting" --limit 2 | jq ". | length")
+  queued_runs=$(gh run list -R "$repo" -w "$workflow_name" --json status -s "queued" --limit 2 | jq ". | length")
 
-  total_runs=$((in_progress_runs + waiting_runs))
+  total_runs=$((in_progress_runs + queued_runs))
 
   if [ "$total_runs" -eq 1 ]; then
     echo "✅ Found one active run (this run itself). Exiting with code 0."
