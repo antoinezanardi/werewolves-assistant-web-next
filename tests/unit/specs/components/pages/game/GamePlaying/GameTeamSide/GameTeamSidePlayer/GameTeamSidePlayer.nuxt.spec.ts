a27b2103bfde6db9ef9494bd7b21050a74944cb9
Antoine Zanardi <antoine.zanardi@epitech.eu>
2024-02-28 11:16:22 +0100

    feat(nuxt-glow): elements can glow (#198)

--- tests/unit/specs/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/GameTeamSidePlayer.nuxt.spec.ts
+++ tests/unit/specs/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/GameTeamSidePlayer.nuxt.spec.ts
@@ -1,4 +1,5 @@
 import type { mount } from "@vue/test-utils";
+import type { ComponentMountingOptions } from "@vue/test-utils/dist/mount";
 
 import type { GameTeamSidePlayerProps } from "~/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/game-team-side-player.types";
 import GameTeamSidePlayer from "~/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/GameTeamSidePlayer.vue";
@@ -11,8 +12,17 @@ describe("Game Team Side Player Component", () => {
   let wrapper: ReturnType<typeof mount<typeof GameTeamSidePlayer>>;
   const defaultProps: GameTeamSidePlayerProps = { player: createFakeWerewolfAlivePlayer({ name: "Antoine" }) };
 
+  async function mountGameTeamSidePlayerComponent(options: ComponentMountingOptions<typeof GameTeamSidePlayer> = {}): Promise<ReturnType<typeof mount<typeof GameTeamSidePlayer>>> {
+    return mountSuspendedComponent(GameTeamSidePlayer, {
+      props: defaultProps,
+      shallow: false,
+      global: { stubs: { RoleImage: true } },
+      ...options,
+    });
+  }
+
   beforeEach(async() => {
-    wrapper = await mountSuspendedComponent(GameTeamSidePlayer, { props: defaultProps });
+    wrapper = await mountGameTeamSidePlayerComponent();
   });
 
   it("should match snapshot when rendered.", () => {
@@ -20,37 +30,55 @@ describe("Game Team Side Player Component", () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  describe("Player name", () => {
-    it("should display player name when rendered.", () => {
-      const playerName = wrapper.find<HTMLDivElement>("#player-name");
-
-      expect(playerName.text()).toBe("Antoine");
-    });
-  });
-
-  describe("Player role name", () => {
-    it("should display player role when rendered.", () => {
-      const playerRole = wrapper.find<HTMLDivElement>("#player-role-name");
-
-      expect(playerRole.text()).toBe("Werewolf");
-    });
-  });
-
-  describe("Player role image", () => {
-    it("should display player role image on the left when side is villagers.", async() => {
+  describe("Game Team Side Player", () => {
+    it("should glow as green when side is villagers.", async() => {
       const player = createFakeSeerAlivePlayer();
-      wrapper = await mountSuspendedComponent(GameTeamSidePlayer, { props: { ...defaultProps, player } });
-      const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-villager-role-image");
+      wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+      const teamSidePlayer = wrapper.find<HTMLDivElement>("#game-team-side-player");
 
-      expect(playerRoleImage.props("roleName")).toBe(RoleNames.SEER);
+      expect(teamSidePlayer.classes("glow:border-green-500")).toBeTruthy();
     });
 
-    it("should display player role image on the right when side is werewolves.", async() => {
+    it("should glow as red when side is werewolves.", async() => {
       const player = createFakeAccursedWolfFatherAlivePlayer();
-      wrapper = await mountSuspendedComponent(GameTeamSidePlayer, { props: { ...defaultProps, player } });
-      const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-werewolf-role-image");
+      wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+      const teamSidePlayer = wrapper.find<HTMLDivElement>("#game-team-side-player");
 
-      expect(playerRoleImage.props("roleName")).toBe(RoleNames.ACCURSED_WOLF_FATHER);
+      expect(teamSidePlayer.classes("glow:border-red-500")).toBeTruthy();
+    });
+
+    describe("Player name", () => {
+      it("should display player name when rendered.", () => {
+        const playerName = wrapper.find<HTMLDivElement>("#player-name");
+
+        expect(playerName.text()).toBe("Antoine");
+      });
+    });
+
+    describe("Player role name", () => {
+      it("should display player role when rendered.", () => {
+        const playerRole = wrapper.find<HTMLDivElement>("#player-role-name");
+
+        expect(playerRole.text()).toBe("Werewolf");
+      });
+    });
+
+    describe("Player role image", () => {
+      it("should display player role image on the left when side is villagers.", async() => {
+        const player = createFakeSeerAlivePlayer();
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-villager-role-image");
+
+        expect(playerRoleImage.props("roleName")).toBe(RoleNames.SEER);
+      });
+
+      it("should display player role image on the right when side is werewolves.", async() => {
+        const player = createFakeAccursedWolfFatherAlivePlayer();
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-werewolf-role-image");
+
+        expect(playerRoleImage.props("roleName")).toBe(RoleNames.ACCURSED_WOLF_FATHER);
+      });
     });
   });
 });
