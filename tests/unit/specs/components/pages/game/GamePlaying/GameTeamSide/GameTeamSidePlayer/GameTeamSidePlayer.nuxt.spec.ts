38153fc2f7a85e5eeb5601b3ad823e2fb31a4696
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-03-09 10:26:05 +0100

    feat(game-team-side-player): change style of player if dead

--- tests/unit/specs/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/GameTeamSidePlayer.nuxt.spec.ts
+++ tests/unit/specs/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/GameTeamSidePlayer.nuxt.spec.ts
@@ -1,6 +1,7 @@
 import type { mount } from "@vue/test-utils";
 import type { ComponentMountingOptions } from "@vue/test-utils/dist/mount";
 
+import type { NuxtImg } from "#components";
 import type { GameTeamSidePlayerProps } from "~/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/game-team-side-player.types";
 import GameTeamSidePlayer from "~/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/GameTeamSidePlayer.vue";
 import GameTeamSidePlayerAttribute from "~/components/pages/game/GamePlaying/GameTeamSide/GameTeamSidePlayer/GameTeamSidePlayerAttribute/GameTeamSidePlayerAttribute.vue";
@@ -9,6 +10,7 @@ import type { PlayerAttribute } from "~/composables/api/game/types/players/playe
 import { RoleNames } from "~/composables/api/role/enums/role.enums";
 import { createFakeActingByActorPlayerAttribute, createFakeSeenBySeerPlayerAttribute } from "~/tests/unit/utils/factories/composables/api/game/player/player-attribute/player-attribute.factory";
 import { createFakeAccursedWolfFatherAlivePlayer, createFakeSeerAlivePlayer, createFakeWerewolfAlivePlayer } from "~/tests/unit/utils/factories/composables/api/game/player/player-with-role.factory";
+import { pTooltipDirectiveBinder } from "~/tests/unit/utils/helpers/directive.helpers";
 import { mountSuspendedComponent } from "~/tests/unit/utils/helpers/mount.helpers";
 
 describe("Game Team Side Player Component", () => {
@@ -62,11 +64,52 @@ describe("Game Team Side Player Component", () => {
       expect(teamSidePlayer.classes("glow:border-red-500")).toBeTruthy();
     });
 
+    it("should not glow when player is dead.", async() => {
+      const player = createFakeSeerAlivePlayer({ isAlive: false });
+      wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+      const teamSidePlayer = wrapper.find<HTMLDivElement>("#game-team-side-player");
+
+      expect(teamSidePlayer.classes()).toStrictEqual<string[]>(["border-2", "border-gray-700", "p-2", "rounded-md", "w-full"]);
+    });
+
+    describe("Player death", () => {
+      it("should display player death logo when player is dead.", async() => {
+        const player = createFakeSeerAlivePlayer({ isAlive: false });
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const deathIcon = wrapper.findComponent<typeof NuxtImg>("[alt='This player is dead']");
+
+        expect(deathIcon.exists()).toBeTruthy();
+      });
+
+      it("should not display player death logo when player is alive.", () => {
+        const deathIcon = wrapper.findComponent<typeof NuxtImg>("[alt='This player is dead']");
+
+        expect(deathIcon.exists()).toBeFalsy();
+      });
+
+      it("should attach tooltip to player death logo when player is dead.", async() => {
+        const tooltip = { value: undefined };
+        const directives = { ...pTooltipDirectiveBinder(tooltip, "[alt='This player is dead']") };
+        const player = createFakeSeerAlivePlayer({ isAlive: false });
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player }, global: { directives } });
+
+        expect(tooltip.value).toBe("This player is dead");
+      });
+    });
+
     describe("Player name", () => {
       it("should display player name when rendered.", () => {
         const playerName = wrapper.find<HTMLDivElement>("#player-name");
 
         expect(playerName.text()).toBe("Antoine");
+      });
+
+      it("should be lined through when player is dead.", async() => {
+        const player = createFakeSeerAlivePlayer({ isAlive: false });
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const playerName = wrapper.find<HTMLDivElement>("#player-name");
+
+        expect(playerName.classes("line-through")).toBeTruthy();
       });
     });
 
@@ -93,6 +136,39 @@ describe("Game Team Side Player Component", () => {
         const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-werewolf-role-image");
 
         expect(playerRoleImage.props("roleName")).toBe(RoleNames.ACCURSED_WOLF_FATHER);
+      });
+
+      it("should have a green border glow class when side is villagers.", async() => {
+        const player = createFakeSeerAlivePlayer();
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-villager-role-image");
+
+        expect(playerRoleImage.classes("glow:border-green-500")).toBeTruthy();
+      });
+
+      it("should have a red border glow class when side is werewolves.", async() => {
+        const player = createFakeAccursedWolfFatherAlivePlayer();
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-werewolf-role-image");
+
+        expect(playerRoleImage.classes("glow:border-red-500")).toBeTruthy();
+      });
+
+      it("should not have border glow class when player is dead.", async() => {
+        const player = createFakeSeerAlivePlayer({ isAlive: false });
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-villager-role-image");
+
+        expect(playerRoleImage.classes("glow:border-green-500")).toBeFalsy();
+        expect(playerRoleImage.classes("glow:border-red-500")).toBeFalsy();
+      });
+
+      it("should have grayscale class when player is dead.", async() => {
+        const player = createFakeSeerAlivePlayer({ isAlive: false });
+        wrapper = await mountGameTeamSidePlayerComponent({ props: { ...defaultProps, player } });
+        const playerRoleImage = wrapper.findComponent<typeof RoleImage>("#player-villager-role-image");
+
+        expect(playerRoleImage.classes("grayscale")).toBeTruthy();
       });
     });
 
