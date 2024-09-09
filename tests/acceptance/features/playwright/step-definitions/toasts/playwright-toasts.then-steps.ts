ede75ee5e1ecab83c30a54d3c7daabfeb70e71e5
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-09-09 09:30:47 +0200

    feat(nuxt): set route announcer

--- tests/acceptance/features/playwright/step-definitions/toasts/playwright-toasts.then-steps.ts
+++ tests/acceptance/features/playwright/step-definitions/toasts/playwright-toasts.then-steps.ts
@@ -4,5 +4,7 @@ import { expect } from "@playwright/test";
 import type { CustomWorld } from "@tests/acceptance/shared/types/word.types";
 
 Then(/^the toast with text "(?<text>.+)" should be visible$/u, async function(this: CustomWorld, text: string): Promise<void> {
-  await expect(this.page.getByRole("alert").getByText(text)).toBeVisible();
+  const toastsContainer = this.page.locator(".p-toast").first();
+
+  await expect(toastsContainer.getByRole("alert").getByText(text)).toBeVisible();
 });
