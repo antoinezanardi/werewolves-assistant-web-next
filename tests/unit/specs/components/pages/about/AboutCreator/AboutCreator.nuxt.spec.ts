000ce26c451a8de29cbd7ee3312db751f8211e93
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2024-09-10 21:01:06 +0200

    fix(eslint): adapt to new eslint stylistic version

--- tests/unit/specs/components/pages/about/AboutCreator/AboutCreator.nuxt.spec.ts
+++ tests/unit/specs/components/pages/about/AboutCreator/AboutCreator.nuxt.spec.ts
@@ -28,7 +28,7 @@ describe("About Creator Component", () => {
       const firstSection = wrapper.find<HTMLParagraphElement>("#about-creator-first-section");
 
       expect(firstSection.text()).toBe("I am Antoine ZANARDI, a Full-Stack Web developer and a fan of open source in general. " +
-      "If you want to learn more about me, I invite you to explore my portfolio.");
+        "If you want to learn more about me, I invite you to explore my portfolio.");
     });
 
     it("should translate portfolio button when rendered.", () => {
@@ -41,22 +41,23 @@ describe("About Creator Component", () => {
       const secondSection = wrapper.find<HTMLParagraphElement>("#about-creator-second-section");
 
       expect(secondSection.text()).toBe("For me, Werewolves of Miller's Hollow™ game is one of the best games to play with friends or " +
-      "family around a table, where alliances and betrayals intertwine, much like in Mario Kart. " +
-      "As the game progresses, the true nature of each player is revealed, masks fall, and everyone feels threatened. Each game is a unique adventure, full of unexpected twists.");
+        "family around a table, where alliances and betrayals intertwine, much like in Mario Kart. " +
+        "As the game progresses, the true nature of each player is revealed, masks fall, and everyone feels threatened. " +
+        "Each game is a unique adventure, full of unexpected twists.");
     });
 
     it("should translate third section when rendered.", () => {
       const thirdSection = wrapper.find<HTMLParagraphElement>("#about-creator-third-section");
 
       expect(thirdSection.text()).toBe("As a big fan of the game master role, I've always wanted to share the rewarding feeling of being " +
-      "the conductor during a game. So, I created this assistant so that anyone can take on this important role, in any game configuration.");
+        "the conductor during a game. So, I created this assistant so that anyone can take on this important role, in any game configuration.");
     });
 
     it("should translate fourth section when rendered.", () => {
       const fourthSection = wrapper.find<HTMLParagraphElement>("#about-creator-forth-section");
 
       expect(fourthSection.text()).toBe("Your feedback on this assistant is welcome. " +
-      "It was created to serve all game masters, whether novice or expert, and make your games memorable. Have fun!");
+        "It was created to serve all game masters, whether novice or expert, and make your games memorable. Have fun!");
     });
   });
 });
